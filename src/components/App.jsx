9d75fd637eee2b648b08413ea00b13634d835a20
Pankumi <111710215+Pankumi@users.noreply.github.com>
2023-02-25 16:08:31 +0200

    Start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
+import { StyledNavLink } from './App.styled'
+const styleApp = {
+  height: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontSize: 40,
+  color: '#010101',
+};
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div style={styleApp}>
+      <header>
+        <nav>
+          <StyledNavLink to="/" end>
+            Home
+          </StyledNavLink>
+          <StyledNavLink to="/about">Movies</StyledNavLink>
+          <StyledNavLink to="/products">Products</StyledNavLink>
+        </nav>
+      </header>
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+      </Routes> */}
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+// // npm install react-router-dom
+// // npm i styled-components
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 );
