5a183cc05940c3e5998fc64af032aafeaac80356
Pankumi <111710215+Pankumi@users.noreply.github.com>
2023-02-28 01:07:46 +0200

    Loader

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
+// // npm install normalize.css
 // // npm install react-router-dom
 // // npm i styled-components
 // // npm install axios
 
+import 'normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import './index.css';
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <React.StrictMode>
+    {/* <React.StrictMode> */}
       <App />
-    </React.StrictMode>
+    {/* </React.StrictMode> */}
   </BrowserRouter>
 );
